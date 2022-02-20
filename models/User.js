9a578fd45d563bceeb79ced5ff477ba04522e2f7
Mike Gordon <vprmatrix55@gmail.com>
2022-02-20 15:04:25 -0700

    password hash on create and update

--- models/User.js
+++ models/User.js
@@ -3,7 +3,31 @@ const sequelize = require("../config/connection");
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password); //Using the keyword this, we can access this user's properties, including the password, which was stored as a hashed string.
+
+    // This is a sync version modeled after format provided from bcrypt
+    // bcrypt.compareSync(myPlaintextPassword, hash); // true
+    // bcrypt.compareSync(someOtherPlaintextPassword, hash); // false
+  }
+    // The bcrypt package offers an async method called compare that takes in two parameters: the plaintext password from the user and 
+    //the hashed password from the database. Then the function compares them. If they match, true is returned. If they don't, false is returned.
+    //For a better user experience on a live app, choose the async version to reduce the time a user has to wait to verify the password
+    
+    // async function checkUser(username, password) {
+    //     //... fetch user from a db etc.
+
+    //     const match = await bcrypt.compare(password, user.passwordHash);
+
+    //     if(match) {
+    //         //login
+    //     }
+
+    //     //...
+    // }
+}
 
 // define table columns and configuration
 User.init(
@@ -58,7 +82,7 @@ User.init(
                         //     return newUserData
                         //     });
                         // }
-                        
+
             // This is the async/await version
         async beforeCreate(newUserData) {
             newUserData.password = await bcrypt.hash(newUserData.password, 10);
